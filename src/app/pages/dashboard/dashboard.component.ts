de89f0684128bd1254a685c934d6d8a2dacb052a
Sanchit Sawardekar <sanchitss7@gmail.com>
2024-10-04 03:30:15 +0530

    Dashboard UI ready

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -10,6 +10,180 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  
   }
+
+  customeremiremider: any = [
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'Rohit shetty',
+      dtemi: '02/01/2025',
+      product: "Vivo T90",
+      emiamount: "5,500",
+      description: 'This is the best product ever!'
+    },
+
+  ];
+
+  bestproduct: any = [
+    {
+      name: 'bestproduct',
+      image: 'https://via.placeholder.com/150',
+      price: 500,
+      qty: 10,
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'bestproduct',
+      image: 'https://via.placeholder.com/150',
+      price: 500,
+      qty: 10,
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'bestproduct',
+      image: 'https://via.placeholder.com/150',
+      price: 500,
+      qty: 10,
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'bestproduct',
+      image: 'https://via.placeholder.com/150',
+      price: 500,
+      qty: 10,
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'bestproduct',
+      image: 'https://via.placeholder.com/150',
+      price: 500,
+      qty: 10,
+      description: 'This is the best product ever!'
+    },
+    {
+      name: 'bestproduct',
+      image: 'https://via.placeholder.com/150',
+      price: 500,
+      qty: 10,
+      description: 'This is the best product ever!'
+    },
+
+  ];
+  bestoffer: any = [
+    {
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    },{
+      name: 'bestoffer',
+      image: 'https://via.placeholder.com/150',
+      dtstart: "12/12/2024",
+      dtend: "31/12/2024",
+      description: 'This is the best offer ever!'
+    }
+  ];
+
+
+
+
 
 }
