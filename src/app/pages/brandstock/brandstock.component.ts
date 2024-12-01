20254a775077d0ae348a01cf1a260b7ae1de1b5f
Neetu330 <neetudas330@gmail.com>
2024-12-02 02:20:18 +0530

    brandstock ui changed

--- src/app/pages/brandstock/brandstock.component.ts
+++ src/app/pages/brandstock/brandstock.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brandstock.component.less']
 })
 export class BrandstockComponent implements OnInit {
+  brands = [
+    { id: 1, status:'In Stock', value: "Samsung" },
+    { id: 2, status:'Sold', value: "Vivo" },
+    { id: 3, status:'Sold', value: "Oppo" },
+    { id: 4, status:'In Stock', value: "Oneplus" },
+    { id: 5, status:'In Stock', value: "Motorola" },
+    { id: 6, status:'In Stock', value: "MI" },
+  ]
 
   constructor() { }
 
