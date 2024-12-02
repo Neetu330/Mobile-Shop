a2e65b727f8db97593f02decdfeb5f1f3ccf152b
Sanchit Sawardekar <sanchitss7@gmail.com>
2024-12-03 02:39:10 +0530

    all pages UI for desktop scree completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'login', component: LoginComponent },
   { path: 'customers', component: CustomersComponent},
-  { path: 'customerdetails/:id', component: CustomerdetailsComponent},
+  { path: 'customerdetails', component: CustomerdetailsComponent},
   { path: 'brandstock', component: BrandstockComponent},
   {
     path: '**',
